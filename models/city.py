9b75fcca74f0bc2d59fad715cff93ce8b84dc715
Edward_Odey <edwardodey04@gmail.com>
2023-11-25 14:42:01 -0800

    mapped the place class to mysql database

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
 from models import HBNB_TYPE_STORAGE
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -12,6 +13,7 @@ class City(BaseModel, Base):
     if HBNB_TYPE_STORAGE == 'db':
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='cities', cascade='all, delete')
     else:
         state_id = ""
         name = ""
