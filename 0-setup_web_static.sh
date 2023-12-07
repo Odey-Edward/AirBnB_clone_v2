eb6e7ca9553d715f89b7717cc3fdc70aeecfcbc1
Edward_Odey <edwardodey04@gmail.com>
2023-12-07 07:02:49 -0800

    update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#a Bash script that sets up your web servers for the deployment of web_static
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
