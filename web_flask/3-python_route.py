fb025ecd310ecc64bd1e3f10c35bf5b960ba62e6
Edward_Odey <edwardodey04@gmail.com>
2023-12-24 10:47:58 -0800

    create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""a module that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnd():
+    """return the string “Hello HBNB!”"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return the string “HBNB”"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """return C followed with a text"""
+    text = text.replace('_', " ")
+    return f'C {text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_func(text='is cool'):
+    """return the string 'python' with some text"""
+    text = text.replace('_', " ")
+    return f'Python {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
