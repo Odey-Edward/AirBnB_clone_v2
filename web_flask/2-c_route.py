7577825670404260dcd6fad458ccaf8106ba12b6
Edward_Odey <edwardodey04@gmail.com>
2023-12-24 10:29:29 -0800

    create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a module that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnd():
+    """return the string “Hello HBNB!”"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return the string “HBNB”"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """return C followed with a text"""
+    text = text.replace('_', " ")
+    return f'C {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
