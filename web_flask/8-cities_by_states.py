337fb1b06fa53465cecde10b2e0c091af045999d
Edward_Odey <edwardodey04@gmail.com>
2023-12-28 09:27:14 -0800

    task8 updated

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A flask web application listening
+on 0.0.0.0, port 5000"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state():
+    """return all cities with thier
+    corresponding state"""
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template('8-cities_by_states.html', states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def cleanup(exc):
+    """clear and close the database
+    connection"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
