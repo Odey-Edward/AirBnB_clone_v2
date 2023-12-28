74f8e1e45b243c4eaa45cca31f2bc4b4c49c4b43
Edward_Odey <edwardodey04@gmail.com>
2023-12-28 03:31:38 -0800

    update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""A flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,6 +9,7 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """serve web page upon request to /states_list URL"""
     all_state = storage.all(State)
     return render_template('7-states_list.html', states=all_state)
 
