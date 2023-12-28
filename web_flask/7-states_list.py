1afe5eefd9346d144e92d16b7fdbf5dbfb0d8dd7
Edward_Odey <edwardodey04@gmail.com>
2023-12-28 03:26:47 -0800

    create 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    #all_state = storage.all(State)
-    return 'Hello Edward'
+    all_state = storage.all(State)
+    return render_template('7-states_list.html', states=all_state)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=500)
