6276fdc63995bdf3a3054bad8d582a501a3c555b
Odey-Edward <edwardodey04@gmail.com>
2024-01-26 17:53:17 +0000

    update template

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,7 +45,6 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/airbnb-dynamic/number_odd_or_even/<int:n>', strict_slashes=False)
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_even(n):
     """display a HTML page only if n is an integer"""
@@ -53,4 +52,4 @@ def odd_even(n):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
